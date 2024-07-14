bbe1eb4496019fa1528057527b5d0ba5be3bb0f4
Jhanjoga <igor.nitrus@hotmail.com>
2024-07-14 11:11:22 -0300

    Initial commit

---
+++ src/main/java/ApiProjeto_nuvem/ProjetoNuvemApplication.java
@@ -0,0 +1,13 @@
+package ApiProjeto_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ApiProjeto_nuvem/ProjetoNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package ApiProjeto_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
